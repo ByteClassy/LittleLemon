fd8691ad1d2507ba8086b113da68279c0f2d288c
ByteClassy <McGittyHub@gmail.com>
2024-11-01 19:17:55 -0400

    Add network fetching and database storage

--- app/src/main/java/com/littlelemon/littlelemon/HomeScreen.kt
+++ app/src/main/java/com/littlelemon/littlelemon/HomeScreen.kt
@@ -1,62 +1,71 @@
 package com.littlelemon.littlelemon
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.testing.TestNavHostController
-
+import androidx.room.Room
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val database = Room.databaseBuilder(context, AppDatabase::class.java, "database").build()
+    val menuItems = database.menuItemDao().getAll().observeAsState(initial = emptyList())
+
     Column(modifier = Modifier.fillMaxWidth()) {
         TopAppBar(navController)
         UpperPanel()
-//        MenuItemsList()
-
+        MenuItemsList(items = menuItems.value)
     }
 }
 
 @Composable
-private fun MenuItemsList(items: List<MenuItemRoom>) {
+fun MenuItemsList(items: List<MenuItemRoom>) {
     LazyColumn(
         modifier = Modifier
             .fillMaxHeight()
             .padding(top = 20.dp)
     ) {
-        items(
-            items = items,
-            itemContent = { menuItem ->
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Text(menuItem.title)
-                    Text(
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(5.dp),
-                        textAlign = TextAlign.Right,
-                        text = "%.2f".format(menuItem.price)
-                    )
-                }
-            }
-        )
+        items(items = items) { menuItem ->
+            MenuItem(menuItem)
+        }
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun MenuItem(menuItem: MenuItemRoom) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Text(menuItem.title)
+        Text(text = "%.2f".format(menuItem.price))
+        Text(menuItem.description)
+        GlideImage(
+            model = menuItem.image,
+            contentDescription = menuItem.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
--- app/src/main/java/com/littlelemon/littlelemon/MainActivity.kt
+++ app/src/main/java/com/littlelemon/littlelemon/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
@@ -26,16 +23,13 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-
 class MainActivity : ComponentActivity() {
-    // Initializes an HTTP client for making network requests, with JSON content negotiation
     private val httpClient = HttpClient(Android) {
         install(ContentNegotiation) {
             json(contentType = ContentType("text", "plain"))
         }
     }
 
-    // Initializes a Room database instance, lazily loaded when needed
     private val database by lazy {
         Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
     }
@@ -44,10 +38,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // Check shared preferences for user data
         val sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
         val isUserOnboarded = sharedPreferences.getBoolean("is_onboarded", false)
-//        isUserOnboarded = false
 
         setContent {
             LittleLemonTheme {
@@ -58,17 +50,10 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding),
                         isUserOnboarded
                     )
-
-                    //////////////////////////
-
-                    //////////////////////////
                 }
             }
         }
-        // Fetches menu items from the network if the database is empty
-        // LifecycleScope is for running coroutines tied to the lifecycle of an Android component, such as an Activity or Fragment.
-        // In this example, the coroutine is launched in the IO dispatcher,
-        // allowing the app to perform network and database operations without blocking the main thread.
+
         lifecycleScope.launch(Dispatchers.IO) {
             if (database.menuItemDao().isEmpty()) {
                 val menuItemsNetwork = fetchMenu()
@@ -77,36 +62,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    // Fetches menu items from the network using the HTTP client
     private suspend fun fetchMenu(): List<MenuItemNetwork> {
         return httpClient
             .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
-            .body<MenuNetwork>() // Parses the response body as a MenuNetwork object
+            .body<MenuNetwork>()
             .menu
     }
 
-    // Saves menu items to the database
     private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
-        // Converts network menu items to Room menu items
         val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
-        // Inserts all menu items into the database
         database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LittleLemonTheme {
-        Greeting("Android")
     }
 }
